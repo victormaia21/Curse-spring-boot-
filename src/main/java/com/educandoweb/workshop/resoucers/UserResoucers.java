485237ef99a29ae3941f3801b2455ab9e967c91d
Victor <jv1555550@gmail.com>
2022-07-19 17:21:18 -0500

    Service layer, component registration

--- src/main/java/com/educandoweb/workshop/resoucers/UserResoucers.java
+++ src/main/java/com/educandoweb/workshop/resoucers/UserResoucers.java
@@ -1,21 +1,38 @@
 package com.educandoweb.workshop.resoucers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.workshop.entities.User;
+import com.educandoweb.workshop.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResoucers {
 	
+	@Autowired
+	UserService service;
+	
 	@GetMapping
-	ResponseEntity<User>findall() {
-		User u = new User(null, "fqwwq", "dqwdq", "wqdqwd", "fwdqwd");
-		return ResponseEntity.ok().body(u);
+	ResponseEntity<List<User>>findall() {
+		List<User> user = service.findall();
+		return ResponseEntity.ok().body(user);
+		}
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<User>findbyid (@PathVariable Integer id) {
+		User user = service.findbyid(id);
+		return ResponseEntity.ok().body(user);
+		
 	}
+	
+	
 	
 
 	
---
+++ src/main/java/com/educandoweb/workshop/service/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.workshop.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.workshop.entities.User;
+import com.educandoweb.workshop.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository repository;
+	
+	public List<User>findall() {
+		List<User>list = repository.findAll();
+		return list;
+	}
+	
+	public User findbyid(Integer id) {
+		Optional<User>user = repository.findById(id);
+		return user.get();
+	}
+}
